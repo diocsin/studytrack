b4ce514a191ccaaa0f72be9ec8d39e15e4424d44
RomanRyskov <rryskov1111@gmail.com>
2025-04-03 19:20:51 +0300

    #study_track_roma_1 класс Student

---
+++ src/model/Student.java
@@ -0,0 +1,40 @@
+package model;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Student {
+    private String id;
+    private String name;
+    private int age;
+    private List<Course> enrolledCourses = new ArrayList<>();
+    private Map<Course, Grade> grades = new HashMap<>();
+
+    public List<Course> getEnrolledCourses() {
+        return enrolledCourses;
+    }
+
+    public void setEnrolledCourses(List<Course> enrolledCourses) {
+        this.enrolledCourses = enrolledCourses;
+    }
+
+    public Map<Course, Grade> getGrades() {
+        return grades;
+    }
+
+    public void setGrades(Map<Course, Grade> grades) {
+        this.grades = grades;
+    }
+
+    public void enroll(Course course) {
+
+    }
+
+    public void assignGrade(Course course, Grade grade) {
+
+    }
+
+    public double calculateAverageGrade() {
+        return 0.0;
+    }
+
+}
