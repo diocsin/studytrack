12c8efe1834b8560168d578c04cdf2f3b57799f7
VurganovaNadezhda <tolpegina1987@list.ru>
2025-04-03 20:31:39 +0300

    #study_track_Nadeshda_1 добавила класс Schedule и метод

--- src/model/Schedule.java
+++ src/model/Schedule.java
@@ -1,19 +1,29 @@
 package model;
 
+import java.sql.Time;
+
 public class Schedule {
     private String dayOfWeek;
     private String time;
     private String room;
 
-
-   public boolean hasSpace() {
-        return false;
+    public String getDayOfWeek() {
+        return dayOfWeek;
     }
 
-   public void enrollStudent(Student student) {
-
+    public String getRoom() {
+        return room;
     }
+
+    public String getTime() {
+        return time;
+    }
+
     public boolean conflictsWith(Schedule other){
+        String dayOfWeek1 = other.dayOfWeek;
+        String time1 = other.time;
+
+
         return false;
     }
 }
