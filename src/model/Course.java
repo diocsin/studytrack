7dd7b487152a5ed7681480d39dae1dc72343ed91
Vital <nurnazarov@rambler.ru>
2025-04-03 19:43:14 +0300

    Study_track_vitalik_1

---
+++ src/model/Course.java
@@ -0,0 +1,26 @@
+package model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Course {
+
+    private String courseId;
+    private String title;
+    private int capacity;
+    private Schedule schedule;
+    private Set<Student> enrolledStudents = new HashSet<>();
+    private Instructor instructor;
+
+    public boolean hasSpace(){
+        return false;
+    }
+
+    public void enrollStudent(Student student){
+
+    }
+
+    boolean conflictsWith(Schedule other){
+        return true;
+    }
+}
---
+++ src/model/Schedule.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Schedule {
+
+    private String dayOfWeek;
+    private String time;
+    private String room;
+}
