cf93fc0e12f6cba80d22766e3729ca2a5896a537
HlushakovaMarina <truhina_m@mail.ru>
2025-04-03 19:57:59 +0300

    #study_track_marina_1 Сделала класс Instructor

---
+++ src/model/Instructor.java
@@ -0,0 +1,62 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class Instructor {
+    private String id;
+
+    private String name;
+
+    private List<Course> courses = new ArrayList<>();
+
+    public Instructor(String id, String name, List<Course> courses) {
+        this.id = id;
+        this.name = name;
+        this.courses = courses;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Instructor that = (Instructor) o;
+        return Objects.equals(courses, that.courses);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(courses);
+    }
+
+    void assignCourse(Course course){
+        course
+        System.out.println("Преподаватель " + name);
+    }
+    List<Course> getCourses(){
+
+    }
+}
