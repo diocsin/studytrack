0f5cf5055f5c3e1ad9478304ea5dab176bd56634
YuliyaBogdanova <juliete.b25@gmail.com>
2025-04-03 20:52:38 +0300

    #study_track_yuliya_1 добавлен StdyManager

---
+++ src/service/StudyManager.java
@@ -0,0 +1,51 @@
+package service;
+
+import exception.DuplicateStudentException;
+import model.Course;
+import model.Grade;
+import model.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudyManager {
+
+    List<Student> students = new ArrayList<>();
+    List<Course> courses = new ArrayList<>();
+
+    public void registerStudent(Student student) {
+        if (!students.contains(student)) {
+            students.add(student);
+            System.out.println("Студент " + student.getName() + " зарегистрирован в системе");
+        } else {
+            throw new DuplicateStudentException(
+                    "студент с таким ID уже есть в системе" + student.getId()
+            );
+        }
+    }
+
+    public void createCourse(Course course) {
+        if (!courses.contains(course)) {
+            courses.add(course);
+            System.out.println("Курс " + course.getTitle() + " создан");
+        }
+    }
+
+    // toDo доделать
+    public void assignInstructor(Course course, Instructor instructor) {
+        instructor.assignCourse(course);
+        course.setInstructor(instructor);
+        System.out.println("Преподаватель " + instructor.getName() + " назначен на курс " + course.getTitle());
+    }
+
+    public void enroll(Student student, Course course) {
+    }
+
+    // toDo доделать
+    public void assignGrade(Student student, Course course, Grade grade) {
+        student.assignGrade(course, grade);
+        System.out.println("Оценка " + grade
+                + " назначена студенту " + student.getName()
+                + " за курс " + course.getTitle());
+    }
+}
