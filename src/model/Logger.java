8e523419b3a66f0cf86db3a40b22ce2cdf1485a0
HlushakovaMarina <truhina_m@mail.ru>
2025-04-07 21:18:02 +0300

    #study_track_marina_1 доработка Logger

--- src/model/Logger.java
+++ src/model/Logger.java
@@ -3,6 +3,7 @@ package model;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 public class Logger {
     private String logFile;
@@ -14,35 +15,31 @@ public class Logger {
     }
 
     public void log(String message) {
+        LocalDateTime time = LocalDateTime.now();
+        String time1 = time.toString();
+        String.format("[%s] [%s] Студент создан", time1, sourceName);
 
+        try (
+                FileWriter fileWriter = new FileWriter("",
+                        true)) {
+            fileWriter.write();
+        } catch (
+                IOException e) {
+            log("Неудалось записать в файл");
+        }
+
+        try (
+                BufferedWriter writer = new BufferedWriter(new
+                        FileWriter(""))) {
+            writer.write("Строка 1");
+            writer.newLine();
+            writer.write("Строка 2");
+        } catch (
+                IOException e) {
+            System.out.println("Неудалось записать в файл");
+        }
     }
 
-    try(
-    FileWriter fileWriter = new FileWriter("",
-            true))
-
-    {
-        fileWriter.write();
-    } catch(
-    IOException e)
-
-    {
-        System.out.println("Неудалось записать в файл");
-    }
-
-                try(
-    BufferedWriter writer = new BufferedWriter(new
-            FileWriter("")))
-
-    {
-        writer.write("Строка 1");
-        writer.newLine();
-        writer.write("Строка 2");
-    } catch(
-    IOException e)
-    {
-        System.out.println("Неудалось записать в файл");
-    }
 
     public String getLogFile() {
         return logFile;
