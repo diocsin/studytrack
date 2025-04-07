748c38ea763f61d609c92344d1ab8ffca6be1592
Roman_Ryskov <rryskov1111@gmail.com>
2025-04-07 10:13:55 +0300

    доделал RegistrationService

--- src/manager/RegistrationService.java
+++ src/manager/RegistrationService.java
@@ -4,6 +4,7 @@ import exception.CourseFullException;
 import exception.DuplicateStudentException;
 import exception.ScheduleConflictException;
 import model.Course;
+import model.Schedule;
 import model.Student;
 
 public class RegistrationService {
@@ -15,6 +16,14 @@ public class RegistrationService {
         }
         if (!course.hasSpace()) {
             throw new CourseFullException("Ошибка: курс " + course.getTitle() + " заполнен");
+        }
+        for (Course enrolledCours : student.getEnrolledCourses()) {
+            if (enrolledCours.getSchedule().conflictsWith(course.getSchedule())){
+                throw  new ScheduleConflictException("Ошибка: конфликт расписания у студента: " + student.getName() + " с курсом "+ course.getTitle());
+            } else {
+                course.enrollStudent(student);
+                student.enroll(course);
+            }
         }
         //Если расписание курса конфликтует с другими курсами студента (использовать schedule.conflictsWith): → Вывести: "Ошибка: конфликт расписания у студента (name) с курсом (title)" → Бросить ScheduleConflictException
         //Иначе: → Вызвать course.enrollStudent(student) → Вызвать student.enroll(course)
