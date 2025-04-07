90a8f18d4e702580a4070a11de6eae12d91cf401
YVBogdanova <ybogdanova3@alfabank.ru>
2025-04-07 13:51:02 +0300

    study_track_Yuliya_2# add StudyManager

---
+++ src/manager/StudyManager.java
@@ -0,0 +1,63 @@
+package manager;
+
+import exception.CourseFullException;
+import exception.DuplicateStudentException;
+import exception.ScheduleConflictException;
+import model.Course;
+import model.Grade;
+import model.Instructor;
+import model.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudyManager {
+
+    List<Student> students =  new ArrayList<>();
+    List<Course> courses =  new ArrayList<>();
+
+    public void registerStudent(Student student) {
+        if (students.contains(student)) {
+            System.out.println("Студент " + student.getName()
+                    + " зарегистрирован в системе");
+        } else students.add(student);
+    }
+
+    public void createCourse(Course course) {
+        if (courses.contains(course)) {
+            System.out.println("Курс " + course.getTitle() + " создан");
+        } else courses.add(course);
+    }
+
+   public void assignInstructor(Course course, Instructor instructor){
+       instructor.assignCourse(course);
+       course.setInstructor(instructor);
+       System.out.println("Преподаватель " + instructor.getName()
+               + " назначен на курс " + course.getTitle());
+   }
+
+    public void enroll(Student student, Course course) {
+        try {
+            RegistrationService.registerStudentToCourse(student, course);
+        } catch (DuplicateStudentException | ScheduleConflictException | CourseFullException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void assignGrade(Student student, Course course, Grade grade) {
+       student.assignGrade(course, grade);
+        System.out.println("Оценка " + grade
+                + " назначена студенту " + student.getName()
+                + " за курс " + course.getTitle());
+    }
+
+
+
+
+
+
+
+
+
+
+}
